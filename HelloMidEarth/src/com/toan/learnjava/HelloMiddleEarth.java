c67f9e2853a94fd4711eca559c601f4c23f08512
Toan Cao2 <caotoanhp23702@gmail.com>
2017-12-05 09:14:34 +0700

    added a pring out two new character

--- HelloMidEarth/src/com/toan/learnjava/HelloMiddleEarth.java
+++ HelloMidEarth/src/com/toan/learnjava/HelloMiddleEarth.java
@@ -3,11 +3,12 @@ package com.toan.learnjava;
 public class HelloMiddleEarth {
 
 	public static void main(String[] args) {
-		String[] names = {"Bilbo", "Frodo", "Aragon", "Legolas", "Gandalf" };
+		String[] names = {"Bilbo", "Frodo", "Aragon", "Legolas", "Gandalf", "boromi", "fafd" };
 		
 		//print greetings
 		for (int i = 0; i < names.length; i++) {
 			System.out.format("Hello, %s!\n", names[i]);
+			System.out.println("How are you doing today?");
 		}
 	}
 
